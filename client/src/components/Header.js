610b9c326bd130bd2edbc79f7fcd1355df88aa5e
arthurcosta <arthurtec29@gmail.com>
2024-06-21 12:46:27 -0300

    mudando o header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "green"}
+          color={showAdd ? "red" : "black"}
           text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
